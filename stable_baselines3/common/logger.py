c08f10fedffdf18e21686e4f71e022689bbb57bc
darklanx <littlecrazymouse.cs06@nctu.edu.tw>
2022-03-01 18:00:03 +0800

    Overwrite SummaryWriter to avoid null logging

--- stable_baselines3/common/logger.py
+++ stable_baselines3/common/logger.py
@@ -14,6 +14,7 @@ from matplotlib import pyplot as plt
 
 try:
     from torch.utils.tensorboard import SummaryWriter
+    from torch.utils.tensorboard.summary import hparams
 except ImportError:
     SummaryWriter = None
 
@@ -23,6 +24,22 @@ WARN = 30
 ERROR = 40
 DISABLED = 50
 
+
+class SummaryWriter(SummaryWriter):
+    def add_hparams(self, hparam_dict, metric_dict):
+        th._C._log_api_usage_once("tensorboard.logging.add_hparams")
+        if type(hparam_dict) is not dict or type(metric_dict) is not dict:
+            raise TypeError('hparam_dict and metric_dict should be dictionary.')
+        exp, ssi, sei = hparams(hparam_dict, metric_dict)
+
+        logdir = self._get_file_writer().get_logdir()
+        
+        with SummaryWriter(log_dir=logdir) as w_hp:
+            w_hp.file_writer.add_summary(exp)
+            w_hp.file_writer.add_summary(ssi)
+            w_hp.file_writer.add_summary(sei)
+            for k, v in metric_dict.items():
+                w_hp.add_scalar(k, v)
 
 class Video(object):
     """
@@ -533,6 +550,14 @@ class Logger(object):
         :param args: log the arguments
         """
         self.log(*args, level=ERROR)
+
+    def get_tb_summary_writer(self):
+        for _format in self.output_formats:
+            if isinstance(_format, TensorBoardOutputFormat):
+                return _format.writer
+        return None
+
+        
 
     # Configuration
     # ----------------------------------------
